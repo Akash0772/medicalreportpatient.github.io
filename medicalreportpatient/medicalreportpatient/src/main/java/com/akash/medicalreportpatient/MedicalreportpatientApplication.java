ff35abfe7e29329b2cd5e7c631b3414910046de1
Akash0772 <akashchaurasia617@gmail.com>
2023-04-15 03:07:08 +0530

    Change

---
+++ medicalreportpatient/medicalreportpatient/src/main/java/com/akash/medicalreportpatient/MedicalreportpatientApplication.java
@@ -0,0 +1,13 @@
+package com.akash.medicalreportpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalreportpatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalreportpatientApplication.class, args);
+	}
+
+}
---
+++ medicalreportpatient/medicalreportpatient/src/test/java/com/akash/medicalreportpatient/MedicalreportpatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.medicalreportpatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalreportpatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
